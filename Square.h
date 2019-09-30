ba7fb01f951d2fed915fe9bc9d545e1ac6e117ed
shamin <shamin99@outlook.com>
2019-09-30 16:51:17 -0600

    added square

---
+++ Square.h
@@ -0,0 +1,19 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <iostream>
+#include <iomanip>
+#include "Point.h"
+#include "Shape.h"
+#include <cstring>
+using namespace std;
+
+class Square: public Shape{
+    private:
+        /* data */
+    public:
+        Square(/* args */);
+        ~Square();
+};
+
+#endif
