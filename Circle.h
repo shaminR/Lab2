e1749d1b61177162063e8acb45847fe05e91d800
avneetgill <avneet2.gill@gmail.com>
2019-10-02 11:35:55 -0600

    implemented circle

---
+++ Circle.h
@@ -0,0 +1,19 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include <iostream>
+#include <iomanip>
+#include "Point.h"
+#include "Square.h"
+#include "Shape.h"
+#include <cstring>
+using namespace std;
+
+class Cricle: public Shape{
+private: 
+    double radius;
+public:
+
+
+}
+#endif
