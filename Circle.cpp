e91e27a367889c69f01243894ec15081b91a7077
avneetgill <avneet2.gill@gmail.com>
2019-10-02 12:47:25 -0600

    circle full implementation

---
+++ Circle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include "Point.h"
+#include "Square.h"
+#include "Shape.h"
+#include <cstring>
+#include <math.h>
+#include "Circle.h"
+using namespace std;
+
+Circle::~Circle(){
+
+}
+
+int main(){
+    Circle c(1.0,2.0,"hehe", 1.0);
+    c.display();
+}
--- Circle.h
+++ Circle.h
@@ -9,11 +9,22 @@
 #include <cstring>
 using namespace std;
 
-class Cricle: public Shape{
-private: 
+class Circle: public Shape{
+protected: 
     double radius;
 public:
+    Circle (double x, double y, const char* name, double rad): Shape(x, y, name), radius(rad){}
+    ~Circle();
+    void display(){
+            Shape::display();
+            cout<<"Radius "<< radius << endl;
+            cout<<"Area: "<<area()<<endl;
+            cout<<"Perimeter: "<<perimeter()<<endl;
+        }
+    double area(){return M_PI * radius * radius;}
+    double perimeter(){return  (2 * M_PI * radius);}
+    double getRadius(){return radius;}
+    void setRadius(double rad){radius = rad;}
+};
 
-
-}
 #endif
